2a2f92204784483c72b0e41f583c7bf715de6359
Aditya Gupta <adityagupta1620@gmail.com>
2025-05-14 15:29:52 +0530

    vite-changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@data': '/src/Data', 
+    },
+  },
   plugins: [
     tailwindcss(),
   ],
